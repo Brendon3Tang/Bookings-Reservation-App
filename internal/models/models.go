a81cb6a7d226def2c675f595c9bbc461a10aafd7
brendon3tang <kyle3tang@gmail.com>
2022-09-16 15:55:20 -0700

    add models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,11 +1,35 @@
 package models
 
+import "time"
+
 // the models package will be used to store things in a database and to get data from database and store them in this model
 
-//Reservation holds reservation data
+// Reservation holds reservation data
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreateAt    time.Time
+	UpdateAt    time.Time
+}
+
+type Rooms struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreateAt    time.Time
+	UpdateAt    time.Time
 }
