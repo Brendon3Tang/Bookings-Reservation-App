b7f84dab0a406d6600f8a6ee943bfac9c6c985fc
brendon3tang <kyle3tang@gmail.com>
2022-09-18 10:27:07 -0700

    set up models

--- internal/models/models.go
+++ internal/models/models.go
@@ -4,32 +4,57 @@ import "time"
 
 // the models package will be used to store things in a database and to get data from database and store them in this model
 
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreateAt    time.Time
+	UpdateAt    time.Time
+}
+
+type Room struct {
+	ID       int
+	RoomName string
+	CreateAt time.Time
+	UpdateAt time.Time
+}
+
+type Restriction struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdateAt        time.Time
+}
+
 // Reservation holds reservation data
 type Reservation struct {
+	ID        int
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreateAt  time.Time
+	UpdateAt  time.Time
+	Room      Room
+	Processed int
 }
 
-type Users struct {
-	ID          int
-	FirstName   string
-	LastName    string
-	Email       string
-	Password    string
-	AccessLevel int
-	CreateAt    time.Time
-	UpdateAt    time.Time
-}
-
-type Rooms struct {
-	ID          int
-	FirstName   string
-	LastName    string
-	Email       string
-	Password    string
-	AccessLevel int
-	CreateAt    time.Time
-	UpdateAt    time.Time
+type RoomRestriction struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreateAt      time.Time
+	UpdateAt      time.Time
+	Room          Room
+	Reservation   Reservation
+	Restriction   Restriction
 }
