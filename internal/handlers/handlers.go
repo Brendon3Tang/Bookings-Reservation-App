2b4ce156029a9dca5d73c38faa1110dab3f5e0e3
brendon3tang <kyle3tang@gmail.com>
2022-09-21 11:12:45 -0700

    clean up reservation page for general room

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -275,9 +275,20 @@ type jsonResponse struct {
 
 // Availability renders the search availability page
 func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	sd := r.Form.Get("start")
+	ed := r.Form.Get("end")
+
+	layout := "2006-01-02"
+	startDate, _ := time.Parse(layout, sd)
+	endDate, _ := time.Parse(layout, ed)
+
+	roomID, _ := strconv.Atoi(r.Form.Get("room_id"))
+
+	available, _ := m.DB.SearchAvailabilityByDatesByRoomID(startDate, endDate, roomID)
+
 	resp := jsonResponse{
-		OK:      false,
-		Message: "Availiable",
+		OK:      available,
+		Message: "",
 	}
 
 	out, err := json.MarshalIndent(resp, "", "     ")
